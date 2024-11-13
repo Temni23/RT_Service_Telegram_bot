2c69b2cdcf798f71352a114cbe49d92f931262a2
Artem Klimov <temni23@yandex.ru>
2024-11-13 21:15:58 +0700

    add google api function upload_information_to_gsheets to api_function

--- api_functions.py
+++ api_functions.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 from yadisk import Client
+from gspread import Client as GClient
+
 
 def upload_and_get_link(client: Client, filename: str, disk_folder: str) -> str:
     """
@@ -12,3 +14,16 @@ def upload_and_get_link(client: Client, filename: str, disk_folder: str) -> str:
         file_url = client.get_download_link(f'/{disk_folder}/{save_filename}')
 
     return file_url
+
+
+
+
+def upload_information_to_gsheets(client: GClient, sheet_name: str) -> None:
+    # Открываем таблицу
+    spreadsheet = client.open(sheet_name)
+
+    # Открываем первый лист
+    worksheet = spreadsheet.sheet1
+
+    # Добавляем строку с данными
+    worksheet.append_row([str(datetime.timestamp(datetime.now())).replace('.', ''), "Имя", "+1234567890"])
--- settings.py
+++ settings.py
@@ -1,8 +1,16 @@
 import os
 import yadisk
 
+from oauth2client.service_account import ServiceAccountCredentials
 from dotenv import load_dotenv
+from gspread import authorize
 
 load_dotenv()
 
+#Создаем клиент яндекса
 YANDEX_CLIENT = yadisk.Client(token=os.getenv('YA_DISK_TOKEN'))
+
+# Устанавливаем соединение с API Google
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_name('test-sheets-441605-f353fc7f5f8c.json', scope)
+GOOGLE_CLIENT = authorize(credentials)
