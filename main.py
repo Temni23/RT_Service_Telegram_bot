716e24aceb49ef0c991665e8a57d53207ce30223
Artem Klimov <temni23@yandex.ru>
2024-11-15 01:06:26 +0700

    add inline keys and answer for random message

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 import os
+from random import choice
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils import executor
@@ -10,29 +12,29 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
 from FSM_Classes import RegistrationStates
+from bots_func import get_main_menu, get_cancel
 from database_functions import is_user_registered, register_user
+from settings import text_message_ansers
 
 load_dotenv()
 
 API_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
-# Initialize bot, storage, and dispatcher
 bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()  # Инициализируем MemoryStorage
-dp = Dispatcher(bot, storage=storage)  # Передаем storage в Dispatcher
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-# Optional: Add logging middleware
-dp.middleware.setup(LoggingMiddleware())  # Логирование для отладки
+dp.middleware.setup(LoggingMiddleware())
 
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     user_id = message.from_user.id
     if is_user_registered('users.db', user_id):
-        await message.reply("Вы уже зарегистрированы! Добро пожаловать!")
+        await message.reply("Добро пожаловать!, я приму вашу заявку",
+                            reply_markup=get_main_menu())
     else:
         keyboard = InlineKeyboardMarkup().add(
             InlineKeyboardButton("Зарегистрироваться",
@@ -43,12 +45,43 @@ async def send_welcome(message: types.Message):
             reply_markup=keyboard
         )
 
+@dp.callback_query_handler(lambda callback: callback.data == 'cancel', state="*")
+async def cmd_cancel(callback: types.CallbackQuery, state: FSMContext) -> None:
+    """
+    Отрабатывает команду cancel и завершает текущее состояние.
+    """
+    # Получаем текущее состояние
+    current_state = await state.get_state()
+
+    # Проверяем, если есть активное состояние, завершаем его
+    if current_state is not None:
+        await state.finish()
+        await callback.message.answer("Вы отменили текущую операцию.", reply_markup=get_main_menu())
+    else:
+        # Если нет активного состояния, выводим сообщение
+        await callback.message.answer("Сейчас нечего отменять. Попробуйте использовать главное меню.", reply_markup=get_main_menu())
+
+    # Удаляем уведомление о нажатии кнопки, чтобы оно не оставалось висеть
+    await callback.answer()
+
+
+
+###############################################################################
+################# Машина состояний регистрация ################################
+###############################################################################
 
 @dp.callback_query_handler(Text(equals="register"))
 async def start_registration(callback_query: types.CallbackQuery):
-    await callback_query.message.answer(
-        "Пожалуйста, введите ваше полное имя (Фамилия Имя Отчество):"
-    )
+    user_id = callback_query.from_user.id
+    if is_user_registered('users.db', user_id):
+        await callback_query.message.answer("Вы уже зарегистрированы! "
+                                            "Я приму вашу заявку",
+                            reply_markup=get_main_menu())
+    else:
+        await callback_query.message.answer(
+            "Пожалуйста, введите ваше полное имя (Фамилия Имя Отчество):",
+            reply_markup=get_cancel()
+        )
     await RegistrationStates.waiting_for_full_name.set()
 
 
@@ -80,7 +113,7 @@ async def get_workplace(message: types.Message, state: FSMContext):
         f"ФИО: {full_name}\n"
         f"Номер телефона: {phone_number}\n"
         f"Место работы: {workplace}\n\n"
-        f"Если все верно, отправьте 'Подтверждаю'."
+        f"Если все верно, нажмите 'Подтверждаю'." # TODO Переделать на кнопку
     )
     await RegistrationStates.next()
 
@@ -103,6 +136,19 @@ async def confirm_registration(message: types.Message, state: FSMContext):
     await message.answer(
         "Вы успешно зарегистрированы и теперь можете пользоваться ботом!")
     await state.finish()
+
+##############################################################################
+##################### Работа с сообщениями
+
+@dp.message_handler()
+async def random_text_message_answer(message: types.Message) -> None:
+    """
+    Функция отправляет случайный ответ из предустановленного списка.
+
+    На текстовое сообщение пользователя.
+    """
+    text = choice(text_message_ansers)
+    await message.reply(text=text, reply_markup=get_main_menu())
 
 
 if __name__ == '__main__':
